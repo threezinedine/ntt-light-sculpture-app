d1a2768e1c4b86d5deb9d6414ae72f67d34c3653
threezinedine <threezinedine@gmail.com>
2025-07-31 08:35:26 +0700

    [config] modify for using python as ui with initial pyqt5 configuration

---
+++ app/application.py
@@ -0,0 +1,32 @@
+import sys
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
+from converted_uis.main_window import Ui_lightSculptureApplication
+
+# ignore the deprecation warning
+import warnings
+
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+
+class LightSculptureMainWindow(QMainWindow):
+    def __init__(
+        self,
+        parent: QWidget | None = None,
+        flags: Qt.WindowType = Qt.WindowType.Widget,
+    ):
+        super().__init__(parent, flags)
+        self.ui = Ui_lightSculptureApplication()
+        self.ui.setupUi(self)
+
+
+class LighSculptureApplication(QApplication):
+    def __init__(self, argv: list[str]):
+        super().__init__(argv)
+        self.main_window = LightSculptureMainWindow()
+
+
+if __name__ == "__main__":
+    app = LighSculptureApplication(sys.argv)
+    app.main_window.showMaximized()
+    sys.exit(app.exec_())
