5ba6c5fdf51fc24f1bb7b4a5894a8a8b792d5baa
threezinedine <threezinedine@gmail.com>
2025-08-08 22:18:15 +0700

    [feature] add event system module

---
+++ app/modules/event_system/event_system.py
@@ -0,0 +1,109 @@
+from typing import Any, Callable, TypeAlias
+from utils.logger import logger
+
+EventCallback: TypeAlias = Callable[..., None]
+
+
+class EventSystem:
+    """
+    Event system is the center hub which store and notify all the event inside the project, this module
+        also support the attaching the event to another one, whereas when the first event is triggered,
+        it also trigger the second event, vice versa, each event will be triggered only once even if it has
+        the circular dependency (but the warning will be logged).
+
+        The event can be registered by the event name and all callbacks which attached to the event will be
+        triggered when the event is triggered.
+
+    Examples:
+    ```python
+        EventSystem.TriggerEvent("event_name", *args, **kwargs) # now no callback is attached to the event and nothing will happen
+
+        EventSystem.RegisterEvent("event_name", callback) # now the callback will be triggered when the event is triggered
+
+        EventSystem.RegisterEvent("event_name", callback) # now the callback will be triggered when the event is triggered
+    ```
+    """
+
+    _callbackMap: dict[str, list[EventCallback]] = {}
+    _eventDependencyMap: dict[str, set[str]] = {}
+
+    _triggeredEventStack: list[str] = (
+        []
+    )  # the stack which is used for checking and avoid the circular dependency trigger, must be empty when the event is triggered, and be cleared after the event is triggered
+
+    @staticmethod
+    def RegisteEvent(eventName: str, callback: EventCallback):
+        """
+        Attach the callback to the current system, whereas if the `eventName` is triggered, the callback will be triggered.
+
+        Args:
+            eventName (str): The name of the event to register. No need to be existed inside the system,
+                but the callback will be triggered when the event is triggered.
+            callback (EventCallback): The callback to be triggered when the event is triggered. This method will
+                received anything.
+        """
+        if eventName not in EventSystem._callbackMap:
+            EventSystem._callbackMap[eventName] = []
+
+        EventSystem._callbackMap[eventName].append(callback)
+
+    @staticmethod
+    def TriggerEvent(eventName: str, *args: Any, **kwargs: Any):
+        """
+        Trigger the event by the event name, whereas all callbacks which attached to the event will be triggered.
+
+        Args:
+            eventName (str): The name of the event to trigger. If the event is not registered, nothing will happen.
+                The warning will be logged.
+        """
+        if eventName not in EventSystem._callbackMap:
+            logger.warning(
+                f'Event "{eventName}" is not registered, nothing will happen.'
+            )
+            return
+
+        EventSystem._triggeredEventStack.clear()
+
+        EventSystem._TriggerEventInternal(eventName, *args, **kwargs)
+
+        EventSystem._triggeredEventStack.clear()
+
+    @staticmethod
+    def _TriggerEventInternal(eventName: str, *args: Any, **kwargs: Any):
+        """
+        The internal method which is used for triggering the event, and the circular dependency will be checked.
+        """
+        if eventName in EventSystem._triggeredEventStack:
+            return
+
+        EventSystem._triggeredEventStack.append(eventName)
+
+        for callback in EventSystem._callbackMap[eventName]:
+            callback(*args, **kwargs)
+
+        if eventName not in EventSystem._eventDependencyMap:
+            return
+
+        for dependencyEventName in EventSystem._eventDependencyMap[eventName]:
+            EventSystem._TriggerEventInternal(dependencyEventName, *args, **kwargs)
+
+    @staticmethod
+    def AttachEvent(eventName: str, *dependencyEventNames: str):
+        """
+        Attach an event to another one, whereas when the first event is triggered, it also trigger the second event.
+            vice versa, each event will be triggered only once even if it has the circular dependency (but the warning will be logged).
+
+        Args:
+            eventName (str): The name of the event to attach. No need to be existed inside the system.
+            *dependencyEventNames (str): The listing of all event which will be attached to the `eventName`.
+        """
+        if eventName not in EventSystem._eventDependencyMap:
+            EventSystem._eventDependencyMap[eventName] = set()
+
+        EventSystem._eventDependencyMap[eventName].update(dependencyEventNames)
+
+        for dependencyEventName in dependencyEventNames:
+            if dependencyEventName not in EventSystem._eventDependencyMap:
+                EventSystem._eventDependencyMap[dependencyEventName] = set()
+
+            EventSystem._eventDependencyMap[dependencyEventName].add(eventName)
---
+++ app/tests/modules/event_system/test_event_system.py
@@ -0,0 +1,186 @@
+import pytest  # type: ignore
+from pytest_mock import MockerFixture
+from unittest.mock import Mock
+from modules.event_system.event_system import EventSystem
+
+TEST_EVENT_NAME = "test_event_name"
+TEST_DEPENDENCY_EVENT_NAME = "test_dependency_event_name"
+TEST_ISOLATED_EVENT_NAME = "test_isolated_event_name"
+
+
+def test_trigger_event_which_is_not_registered(mocker: MockerFixture):
+    warningMock = mocker.patch("utils.logger.logger.warning")
+    EventSystem.TriggerEvent(TEST_EVENT_NAME)
+    warningMock.assert_called_once()
+
+
+def test_non_registered_event_will_be_not_triggered_when_event_is_triggered():
+    callback = Mock()
+    EventSystem.TriggerEvent(TEST_EVENT_NAME)
+    assert callback.called == 0
+
+
+def test_register_event_will_be_triggered_when_event_is_triggered():
+    callback = Mock()
+    EventSystem.RegisteEvent(TEST_EVENT_NAME, callback)
+    EventSystem.TriggerEvent(TEST_EVENT_NAME)
+    assert callback.called == 1
+
+
+def test_multiple_callbacks_will_be_triggered_when_event_is_triggered():
+    callback1 = Mock()
+    callback2 = Mock()
+    EventSystem.RegisteEvent(TEST_EVENT_NAME, callback1)
+    EventSystem.RegisteEvent(TEST_EVENT_NAME, callback2)
+    EventSystem.TriggerEvent(TEST_EVENT_NAME)
+    assert callback1.called == 1
+    assert callback2.called == 1
+
+
+def test_only_registered_callback_will_be_triggered_when_event_is_triggered():
+    callback1 = Mock()
+    callback2 = Mock()
+    EventSystem.RegisteEvent(TEST_EVENT_NAME, callback1)
+    EventSystem.TriggerEvent(TEST_EVENT_NAME)
+    assert callback1.called == 1
+    assert callback2.called == 0
+
+
+def test_the_depedency_event_will_be_triggered_when_the_top_event_is_triggered():
+    callback1 = Mock()
+    callback2 = Mock()
+    EventSystem.RegisteEvent(TEST_EVENT_NAME, callback1)
+    EventSystem.RegisteEvent(TEST_DEPENDENCY_EVENT_NAME, callback2)
+
+    EventSystem.AttachEvent(TEST_EVENT_NAME, TEST_DEPENDENCY_EVENT_NAME)
+
+    EventSystem.TriggerEvent(TEST_EVENT_NAME)
+
+    assert callback1.called == 1
+    assert callback2.called == 1
+
+
+def test_the_circular_dependency_will_be_logged_when_the_event_is_triggered():
+    callback1 = Mock()
+    callback2 = Mock()
+    EventSystem.RegisteEvent(TEST_EVENT_NAME, callback1)
+    EventSystem.RegisteEvent(TEST_DEPENDENCY_EVENT_NAME, callback2)
+
+    EventSystem.AttachEvent(TEST_EVENT_NAME, TEST_DEPENDENCY_EVENT_NAME)
+    EventSystem.AttachEvent(TEST_DEPENDENCY_EVENT_NAME, TEST_EVENT_NAME)
+
+    EventSystem.TriggerEvent(TEST_EVENT_NAME)
+
+    assert callback1.called == 1
+    assert callback2.called == 1
+
+
+def test_the_circular_dependency_with_an_isolated_event():
+    callback1 = Mock()
+    callback2 = Mock()
+    callback3 = Mock()
+    EventSystem.RegisteEvent(TEST_EVENT_NAME, callback1)
+    EventSystem.RegisteEvent(TEST_DEPENDENCY_EVENT_NAME, callback2)
+    EventSystem.RegisteEvent(TEST_ISOLATED_EVENT_NAME, callback3)
+
+    EventSystem.AttachEvent(TEST_EVENT_NAME, TEST_DEPENDENCY_EVENT_NAME)
+
+    EventSystem.TriggerEvent(TEST_DEPENDENCY_EVENT_NAME)
+
+    assert callback1.called == 1
+    assert callback2.called == 1
+    assert callback3.called == 0
+
+
+def test_attach_multiple_events_to_the_same_event():
+    callback1 = Mock()
+    callback2 = Mock()
+    callback3 = Mock()
+    EventSystem.RegisteEvent(TEST_EVENT_NAME, callback1)
+    EventSystem.RegisteEvent(TEST_DEPENDENCY_EVENT_NAME, callback2)
+    EventSystem.RegisteEvent(TEST_ISOLATED_EVENT_NAME, callback3)
+
+    EventSystem.AttachEvent(
+        TEST_EVENT_NAME,
+        TEST_DEPENDENCY_EVENT_NAME,
+        TEST_ISOLATED_EVENT_NAME,
+    )
+
+    EventSystem.TriggerEvent(TEST_EVENT_NAME)
+
+    assert callback1.called == 1
+    assert callback2.called == 1
+    assert callback3.called == 1
+
+
+def test_attach_multiple_events_so_when_modify_non_root_event_will_trigger_all_the_events():
+    callback1 = Mock()
+    callback2 = Mock()
+    callback3 = Mock()
+    EventSystem.RegisteEvent(TEST_EVENT_NAME, callback1)
+    EventSystem.RegisteEvent(TEST_DEPENDENCY_EVENT_NAME, callback2)
+    EventSystem.RegisteEvent(TEST_ISOLATED_EVENT_NAME, callback3)
+
+    EventSystem.AttachEvent(
+        TEST_EVENT_NAME,
+        TEST_DEPENDENCY_EVENT_NAME,
+        TEST_ISOLATED_EVENT_NAME,
+    )
+
+    EventSystem.TriggerEvent(TEST_DEPENDENCY_EVENT_NAME)
+
+    assert callback1.called == 1
+    assert callback2.called == 1
+    assert callback3.called == 1
+
+
+def test_complex_callback_dependency():
+    callback1 = Mock()
+    callback2 = Mock()
+    callback3 = Mock()
+    EventSystem.RegisteEvent(TEST_EVENT_NAME, callback1)
+    EventSystem.RegisteEvent(TEST_DEPENDENCY_EVENT_NAME, callback2)
+    EventSystem.RegisteEvent(TEST_ISOLATED_EVENT_NAME, callback3)
+
+    EventSystem.AttachEvent(TEST_EVENT_NAME, TEST_DEPENDENCY_EVENT_NAME)
+    EventSystem.AttachEvent(TEST_DEPENDENCY_EVENT_NAME, TEST_ISOLATED_EVENT_NAME)
+    EventSystem.AttachEvent(TEST_ISOLATED_EVENT_NAME, TEST_EVENT_NAME)
+
+    EventSystem.TriggerEvent(TEST_EVENT_NAME)
+
+    assert callback1.called == 1
+    assert callback2.called == 1
+    assert callback3.called == 1
+
+
+def test_multiple_callback_with_the_same_event_name():
+    callback1 = Mock()
+    callback2 = Mock()
+    EventSystem.RegisteEvent(TEST_EVENT_NAME, callback1)
+    EventSystem.RegisteEvent(TEST_EVENT_NAME, callback2)
+
+    EventSystem.TriggerEvent(TEST_EVENT_NAME)
+
+    assert callback1.called == 1
+    assert callback2.called == 1
+
+
+def test_dependency_without_callback_is_also_triggered():
+    callback1 = Mock()
+    EventSystem.RegisteEvent(TEST_EVENT_NAME, callback1)
+
+    EventSystem.AttachEvent(TEST_EVENT_NAME, TEST_DEPENDENCY_EVENT_NAME)
+
+    EventSystem.TriggerEvent(TEST_DEPENDENCY_EVENT_NAME)
+
+    assert callback1.called == 1
+
+
+def test_call_the_event_with_arguments():
+    callback1 = Mock()
+    EventSystem.RegisteEvent(TEST_EVENT_NAME, callback1)
+
+    EventSystem.TriggerEvent(TEST_EVENT_NAME, "test_argument1", "test_argument2")
+
+    assert callback1.called == 1
+    assert callback1.call_args.args == ("test_argument1", "test_argument2")
